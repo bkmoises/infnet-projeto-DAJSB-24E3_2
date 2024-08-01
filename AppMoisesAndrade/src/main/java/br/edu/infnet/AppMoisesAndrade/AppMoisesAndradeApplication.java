3e7623746d0165a6ec63d18cdabbcc611944ba76
bkmoises <bk_moises@hotmail.com>
2024-08-01 12:29:48 -0300

    Initial commit

---
+++ AppMoisesAndrade/src/main/java/br/edu/infnet/AppMoisesAndrade/AppMoisesAndradeApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppMoisesAndrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMoisesAndradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMoisesAndradeApplication.class, args);
+	}
+
+}
